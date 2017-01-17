5cad585906e3328be2f56ff5c8fa1481424b8c9d
apple <chgpgf@gamil.com>
2017-01-17 23:45:53 +0800

    implement part 2

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,7 +1,7 @@
 class Admin::JobsController < ApplicationController
 
   before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-
+  before_filter :require_is_admin
   def index
     @jobs = Job.all
   end
@@ -44,6 +44,8 @@ class Admin::JobsController < ApplicationController
 
     redirect_to admin_jobs_path
   end
+
+  
 
   private
 
